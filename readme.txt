d828c7dce9ba80d3a625d8b910b911e614dd3af6
royaltechbd <royaltechbd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-01 03:48:26 +0000

    Initial
    git-svn-id: https://plugins.svn.wordpress.org/rt-prettyphoto/trunk@749061 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== PPM Lightbox ===
+Contributors: royaltechbd
+Donate link: 
+Tags: jQuery Lightbox, jQuery PrettyPhoto, Lightbox
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin will automatic add lightbox in wordpress post/page without disturbance.
+
+
+== Description ==
+
+This plugin will automatic add lightbox in wordpress post/page without disturbance.
+
+### Plugin Features
+* Easy to use.
+* Support Images, Youtube Video, Vimeo video, iFrame, DEV. 
+
+
+== Installation ==
+
+
+1. Upload the plugin to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add Images as like you upload (with media link)
+
+
+
+== Frequently asked questions ==
+
+= Is it need rel="" =
+
+No, Just add photo, It autometic add rel="".
+
+
+== Screenshots ==
+1. Royal PrettyPhoto in action.
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+
+== Upgrade notice ==
+= 1.0 =
+It is Initial release
---
+++ rt-prettyphoto.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: Royal PrettyPhoto
+Plugin URI: httphttp://wordpress.org/plugins/wp-prettyphoto
+Description: This plugin will automatic add lightbox in wordpress post/page without disturbance.
+Author: Mehdi Akram
+Author URI: http://shamokaldarpon.com
+Version: 1.0
+*/
+
+add_filter('the_content', 'royal_prettyphoto_replace', 12);
+add_filter('get_comment_text', 'royal_prettyphoto_replace');
+function royal_prettyphoto_replace ($content)
+{   global $post;
+	$pattern = "/<a(.*?)href=('|\")([^>]*).(bmp|gif|jpeg|jpg|png)('|\")(.*?)>(.*?)<\/a>/i";
+    $replacement = '<a$1href=$2$3.$4$5 rel="prettyPhoto['.$post->ID.']"$6>$7</a>';
+    $content = preg_replace($pattern, $replacement, $content);
+    return $content;
+}
+
+/*Some Set-up*/
+define('RT_WPP_PLUGIN_PATH', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
+
+
+/* Adding Latest jQuery from Wordpress */
+function rt_wpp_latest_jquery() {
+	wp_enqueue_script('jquery');
+}
+add_action('init', 'rt_wpp_latest_jquery');
+
+
+
+/* Adding plugin javascript active file */
+wp_enqueue_script('rt-wpp-plugin-active', RT_WPP_PLUGIN_PATH.'js/jquery.prettyPhoto.js', array('jquery'));
+/* Adding plugin javascript active file */
+wp_enqueue_script('rt-wpp-plugin-script-active', RT_WPP_PLUGIN_PATH.'js/wpp-active.js', array('jquery'));
+
+/* Adding Plugin custm CSS file */
+wp_enqueue_style('rt-wpp-plugin-style', RT_WPP_PLUGIN_PATH.'css/prettyPhoto.css');
+
+
+
+?>
